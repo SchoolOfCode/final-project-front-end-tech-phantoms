ddeaffc3ee1b64d1174b09de0af24bca8e8cd21f
eniabellver <eugeniabellver@gmail.com>
2022-02-23 12:14:49 +0000

    testing login session for different pages

---
+++ pages/profile.js
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0";
+
+export default function Home() {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
+  if (user) {
+    return (
+      <div>
+        <p>
+          Welcome, {user.name}!
+          <br />
+          Your name is {user.name}, your email is {user.email}.
+        </p>
+        <Link href="/api/auth/[...auth0]" as="/api/auth/logout">
+          <a>LOGOUT</a>
+        </Link>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <main>
+        <p>You are not logged in.</p>
+        <Link href="/api/auth/[...auth0]" as="/api/auth/login">
+          <a>LOGIN</a>
+        </Link>
+      </main>
+    </div>
+  );
+}
