f2255e1239f11b5478716b3b419b8535a27c58a7
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-02-25 15:52:21 +0000

    added a health label image to each recipe

--- Components/RecipeCard/index.js
+++ Components/RecipeCard/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import Image from "next/image";
 
 const RecipeCard = ({ recipe }) => {
@@ -12,8 +12,17 @@ const RecipeCard = ({ recipe }) => {
           src={recipe.image}
         ></Image>
       </div>
+
       <div>
         <h2>{recipe.label}</h2>
+        {recipe.healthLabels.includes("Vegetarian") && (
+          <Image
+            height={50}
+            width={50}
+            alt="Health Label"
+            src="https://cdn.icon-icons.com/icons2/2108/PNG/512/vegetarian_icon_130798.png"
+          />
+        )}
       </div>
     </div>
   );
