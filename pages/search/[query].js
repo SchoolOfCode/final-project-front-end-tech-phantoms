0062cce4aa91e519941d4aae3f6ff5c06835136d
bM7tcHF88GBxDni <bM7tcHF88GBxDni@protonmail.com>
2022-02-25 12:34:23 +0000

    Implement query fetch for search results

--- pages/search/[query].js
+++ pages/search/[query].js
@@ -2,22 +2,34 @@ import React from "react";
 import NavBar from "../../Components/NavBar";
 import SearchBar from "../../Components/SearchBar";
 
-function searchResults({ query }) {
+function searchResults({ data }) {
   return (
     <>
       <NavBar />
       <SearchBar></SearchBar>
-      <p>{"Search:" + JSON.stringify(query)}</p>
+      <p>{"Fetch Results:" + JSON.stringify(data)}</p>
     </>
   );
 }
 
 //https://nextjs.org/docs/basic-features/pages#scenario-1-your-page-content-depends-on-external-data
 export async function getServerSideProps(context) {
+  //create fetch URL
+  //URL encode
+  const fetchURL =
+    `https://api.edamam.com/api/recipes/v2?type=public&q=` +
+    encodeURI(context.params.query) +
+    `&app_id=` +
+    process.env.EDAMAM_APP_ID +
+    `&app_key=` +
+    process.env.EDAMAM_APP_KEY;
+
+  const res = await fetch(fetchURL);
+  const data = await res.json();
+
+  //feed our component props at pre-render time
   return {
-    props: {
-      query: JSON.parse(JSON.stringify(context.params.query)),
-    },
+    props: { data },
   };
 }
 
