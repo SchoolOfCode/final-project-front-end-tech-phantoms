09a65113f0b7deecab7aa5144ccfeb0c6cc70f02
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-02-25 16:01:17 +0000

    added a cook time to each result

--- Components/RecipeCard/index.js
+++ Components/RecipeCard/index.js
@@ -15,6 +15,17 @@ const RecipeCard = ({ recipe }) => {
 
       <div>
         <h2>{recipe.label}</h2>
+        <Image
+          height={50}
+          width={50}
+          alt="Recipe Time"
+          src="https://cdn.icon-icons.com/icons2/2098/PNG/512/clock_icon_128908.png"
+        />
+        {recipe.totalTime > 0 ? (
+          <p>{recipe.totalTime + "'"} </p>
+        ) : (
+          <p>{"20'"}</p>
+        )}
         {recipe.healthLabels.includes("Vegetarian") && (
           <Image
             height={50}
