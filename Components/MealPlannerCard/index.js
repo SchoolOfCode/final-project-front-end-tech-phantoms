6887c31110d0108d5a77b9f541c2085eab95bfee
The-Six <ijazdriscoll@gmail.com>
2022-03-02 16:19:39 +0000

    meal planner modal/display data in progress

--- Components/MealPlannerCard/index.js
+++ Components/MealPlannerCard/index.js
@@ -1,18 +1,18 @@
 import React, { useEffect } from "react";
 import Image from "next/image";
 import css from "../style.module.css";
-import {
-  breakfast as bf,
-  lunch as lh,
-  dinner as dr,
-} from "../../mealPlannerDb";
 import { useState } from "react";
 import AddMealModal from "../AddMealModal";
+import RecipeDayCard from "../RecipeDayCard";
 
-function MealPlannerCard() {
-  const [breakfast, setBreakfast] = useState(bf);
-  const [lunch, setLunch] = useState(lh);
-  const [dinner, setDinner] = useState(dr);
+export async function getServerSideProps(context) {
+    const res = await fetch("http://localhost:3001/savedRecipes")
+    const data = await res.json
+    return { props: {data: data} }
+}
+
+function MealPlannerCard({ data }) {
+
   const [show, setShow] = useState(false);
 
 
@@ -20,98 +20,9 @@ function MealPlannerCard() {
       setShow(true);
   }
 
-  function handleDeleteBf(id) {
-    const newBf = breakfast.filter((item) => {
-      return item.linkToInstructions !== id;
-    });
-    setBreakfast(newBf);
-  }
-  function handleDeleteLh(id) {
-    const newLh = lunch.filter((item) => {
-      return item.linkToInstructions !== id;
-    });
-    setLunch(newLh);
-  }
-  function handleDeleteDr(id) {
-    const newDr = dinner.filter((item) => {
-      return item.linkToInstructions !== id;
-    });
-    setDinner(newDr);
-  }
-  return (
-    <div className={css.container}>
-      {breakfast.map((meal) => {
-        return (
-          <div key={meal.linkToInstructions}>
-            <h3>{meal.recipeName}</h3>
-            <Image
-              src={meal.recipeImage}
-              alt={meal.recipeName}
-              height={100}
-              width={100}
-            />
-            <button>
-              <a href={meal.linkToInstructions}> Method</a>
-            </button>
-            <button
-              onClick={() => {
-                handleDeleteBf(meal.linkToInstructions);
-              }}
-            >
-              delete
-            </button>
-          </div>
-        );
-      })}
-      {lunch.map((meal) => {
-        return (
-          <div key={meal.linkToInstructions}>
-            <h3>{meal.recipeName}</h3>
-            <Image
-              src={meal.recipeImage}
-              alt={meal.recipeName}
-              height={100}
-              width={100}
-            />
-            <button>
-              <a href={meal.linkToInstructions}> Method</a>
-            </button>
-            <button
-              onClick={() => {
-                handleDeleteLh(meal.linkToInstructions);
-              }}
-            >
-              delete
-            </button>
-          </div>
-        );
-      })}
-      {dinner.map((meal) => {
-        return (
-          <div key={meal.linkToInstructions}>
-            <h3>{meal.recipeName}</h3>
-            <Image
-              src={meal.recipeImage}
-              alt={meal.recipeName}
-              height={100}
-              width={100}
-            />
-            <button>
-              <a href={meal.linkToInstructions}> Method</a>
-            </button>
-            <button
-              onClick={() => {
-                handleDeleteDr(meal.linkToInstructions);
-              }}
-            >
-              delete
-            </button>
-          </div>
-        );
-      })}
-      <button>Add</button>
-    </div>
-  );
+  
+ 
+  return <>{data.map(() =>{<RecipeDayCard recipe={data}/>})}</> //we need to map through the recipe day card
 }
 
 export default MealPlannerCard;
---
+++ Components/RecipeDayCard/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RecipeDayCard({recipe}) {
+  return (
+    <div>{recipe.map}</div>
+  )
+}
+
+export default RecipeDayCard
