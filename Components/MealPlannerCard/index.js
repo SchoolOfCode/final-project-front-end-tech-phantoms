ecf167ab4a7b86d9c182c957395783fa0c6a28e2
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-03-02 13:40:39 +0000

    consolidated saved recipes db

--- Components/MealPlannerCard/index.js
+++ Components/MealPlannerCard/index.js
@@ -1,87 +1,112 @@
-import React from 'react'
+import React, { useEffect } from "react";
 import Image from "next/image";
 import css from "../style.module.css";
-import { breakfast as bf, lunch as lh, dinner as dr } from "../../mealPlannerDb";
-import { useState } from "react"
-
-
+import {
+  breakfast as bf,
+  lunch as lh,
+  dinner as dr,
+} from "../../mealPlannerDb";
+import { useState } from "react";
 
 function MealPlannerCard() {
-    const [breakfast, setBreakfast]= useState(bf);
-    const [lunch, setLunch]= useState(lh);
-    const [dinner, setDinner]= useState(dr);
-    function handleDeleteBf(id){
-        // we need to figure this one out
-        const newBf = breakfast.filter((item)=>{return item.linkToInstructions !== id;});
-        setBreakfast(newBf);
-    }
-    function handleDeleteLh(id){
-        // we need to figure this one out
-        const newLh = lunch.filter((item)=>{return item.linkToInstructions !== id;});
-        setLunch(newLh);
-    }
-    function handleDeleteDr(id){
-        // we need to figure this one out
-        const newDr = dinner.filter((item)=>{return item.linkToInstructions !== id;});
-        setDinner(newDr);
-        
-    }
+  const [breakfast, setBreakfast] = useState(bf);
+  const [lunch, setLunch] = useState(lh);
+  const [dinner, setDinner] = useState(dr);
+
+  function handleAdd() {}
+
+  function handleDeleteBf(id) {
+    const newBf = breakfast.filter((item) => {
+      return item.linkToInstructions !== id;
+    });
+    setBreakfast(newBf);
+  }
+  function handleDeleteLh(id) {
+    const newLh = lunch.filter((item) => {
+      return item.linkToInstructions !== id;
+    });
+    setLunch(newLh);
+  }
+  function handleDeleteDr(id) {
+    const newDr = dinner.filter((item) => {
+      return item.linkToInstructions !== id;
+    });
+    setDinner(newDr);
+  }
   return (
     <div className={css.container}>
-        {breakfast.map((meal) => {
-          return (
-            <div key={meal.linkToInstructions}>
-              <h3>{meal.recipeName}</h3>
-              <Image
-                src={meal.recipeImage}
-                alt={meal.recipeName}
-                height={100}
-                width={100}
-              />
-              <button>
-                <a href={meal.linkToInstructions}> Method</a>
-              </button>
-              <button onClick={()=>{handleDeleteBf(meal.linkToInstructions)}}>delete</button>
-              
-            </div>
-          );
-        })}
-        {lunch.map((meal) => {
-          return (
-            <div key={meal.linkToInstructions}>
-              <h3>{meal.recipeName}</h3>
-              <Image
-                src={meal.recipeImage}
-                alt={meal.recipeName}
-                height={100}
-                width={100}
-              />
-              <button>
-                <a href={meal.linkToInstructions}> Method</a>
-              </button>
-              <button onClick={()=>{handleDeleteLh(meal.linkToInstructions)}}>delete</button>
-            </div>
-          );
-        })}
-        {dinner.map((meal) => {
-          return (
-            <div key={meal.linkToInstructions}>
-              <h3>{meal.recipeName}</h3>
-              <Image
-                src={meal.recipeImage}
-                alt={meal.recipeName}
-                height={100}
-                width={100}
-              />
-              <button>
-                <a href={meal.linkToInstructions}> Method</a>
-              </button>
-              <button onClick={()=>{handleDeleteDr(meal.linkToInstructions)}}>delete</button>
-            </div>
-          );
-        })}
-      </div>
-  )
+      {breakfast.map((meal) => {
+        return (
+          <div key={meal.linkToInstructions}>
+            <h3>{meal.recipeName}</h3>
+            <Image
+              src={meal.recipeImage}
+              alt={meal.recipeName}
+              height={100}
+              width={100}
+            />
+            <button>
+              <a href={meal.linkToInstructions}> Method</a>
+            </button>
+            <button
+              onClick={() => {
+                handleDeleteBf(meal.linkToInstructions);
+              }}
+            >
+              delete
+            </button>
+          </div>
+        );
+      })}
+      {lunch.map((meal) => {
+        return (
+          <div key={meal.linkToInstructions}>
+            <h3>{meal.recipeName}</h3>
+            <Image
+              src={meal.recipeImage}
+              alt={meal.recipeName}
+              height={100}
+              width={100}
+            />
+            <button>
+              <a href={meal.linkToInstructions}> Method</a>
+            </button>
+            <button
+              onClick={() => {
+                handleDeleteLh(meal.linkToInstructions);
+              }}
+            >
+              delete
+            </button>
+          </div>
+        );
+      })}
+      {dinner.map((meal) => {
+        return (
+          <div key={meal.linkToInstructions}>
+            <h3>{meal.recipeName}</h3>
+            <Image
+              src={meal.recipeImage}
+              alt={meal.recipeName}
+              height={100}
+              width={100}
+            />
+            <button>
+              <a href={meal.linkToInstructions}> Method</a>
+            </button>
+            <button
+              onClick={() => {
+                handleDeleteDr(meal.linkToInstructions);
+              }}
+            >
+              delete
+            </button>
+          </div>
+        );
+      })}
+      <button>Add</button>
+    </div>
+  );
 }
 
-export default MealPlannerCard
+export default MealPlannerCard;
