f17c02a4da4ad782c1ccdc6c8304387ddda0e7ec
scottwilson <swrs@hotmail.co.uk>
2022-03-02 16:01:06 +0000

    added postSavedRecipe function

--- Components/SaveRecipeButton/index.js
+++ Components/SaveRecipeButton/index.js
@@ -8,21 +8,42 @@ import { useUser } from "@auth0/nextjs-auth0";
 function SaveRecipeButton({ recipeID }) {
   const { user } = useUser();
 
-  function handleClick() {
-    console.log(`clicked, id is: ${recipeID}`); //DEBUG LOG REFACTOR AFTER
+  async function postSavedRecipe() {
+    const email = user.email;
+    const uri =
+      `https://dev-backend-phantom-kitchen.herokuapp.com/users/` +
+      email +
+      `/favourites/` +
+      recipeID;
+    const message = await fetch(uri, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+    const content = await message.json();
+    console.log(content);
   }
 
-  
+  async function handleClick() {
+    await postSavedRecipe();
+    console.log(`clicked, id is: ${recipeID}`); //DEBUG LOG REFACTOR AFTER
+  }
 
   if (user) {
     return <button onClick={handleClick}>Save recipe</button>;
   }
 
-  return (<>
+  return (
+    <>
       <Link href="/api/auth/[...auth0]" as="/api/auth/login">
-        <a><button>Login to save</button></a>
+        <a>
+          <button>Login to save</button>
+        </a>
       </Link>
-  </>)
+    </>
+  );
 }
 
 export default SaveRecipeButton;
