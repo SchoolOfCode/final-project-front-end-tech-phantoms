862e2b021f1bb916b0cab98838d5a95a46260938
eniabellver <eugeniabellver@gmail.com>
2022-03-02 12:02:35 +0000

    added image to recipe details

--- Components/RecipeDetails/index.js
+++ Components/RecipeDetails/index.js
@@ -2,15 +2,17 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
-//TODO: Once the data has been fetched get it as a prop
-// access the data to replace our mock data
-//TODO: Create save recipe button, that calls a function to do a patch request.
-
 const RecipeDetails = ({ onClick, data }) => {
-  console.log(data);
   return (
     <div>
-      {/* <Image alt="Recipe banner"></Image> */}
+      <Image
+        alt="Recipe banner"
+        src={data.image}
+        width="100%"
+        height="100%"
+        layout="responsive"
+        objectFit="contain"
+      ></Image>
       <h1>{data.label}</h1>
       <hr />
       <h2>Nutrition</h2>
@@ -19,10 +21,26 @@ const RecipeDetails = ({ onClick, data }) => {
         <br />
         <br />
         <span>{Math.round(data.totalDaily.ENERC_KCAL.quantity)}%</span> daily
-        value
+        calorie value
+        <Link
+          href="https://www.nhs.uk/common-health-questions/food-and-diet/what-should-my-daily-intake-of-calories-be/"
+          passHref
+        >
+          <a target="_blank"> &#9432;</a>
+        </Link>
         <br />
         <br />
         <span>{data.healthLabels.join(", ")}</span>
+        <br />
+        <br />
+        {data.healthLabels.includes("Vegetarian") && (
+          <Image
+            height={30}
+            width={30}
+            alt="Health Label"
+            src="https://cdn.icon-icons.com/icons2/2108/PNG/512/vegetarian_icon_130798.png"
+          />
+        )}
       </p>
       <h2>Ingredients</h2>
       <ul>
@@ -32,7 +50,9 @@ const RecipeDetails = ({ onClick, data }) => {
       </ul>
       <h2>Preparation</h2>
       <Link href={data.url} passHref>
-        <button>Read Instructions</button>
+        <a target="_blank">
+          <button>Read Instructions</button>
+        </a>
       </Link>
       <p>on {data.source}</p>
       <hr />
