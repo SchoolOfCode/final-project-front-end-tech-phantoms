a0391a59c60c2f554d6582b69aac6014a3ea6efa
eniabellver <eugeniabellver@gmail.com>
2022-03-08 11:38:58 +0000

    added some css and scaffolding for deleteItem functionality

--- Components/ShoppingItem/index.js
+++ Components/ShoppingItem/index.js
@@ -1,15 +1,18 @@
 import React from "react";
-import { Checkbox } from "@nextui-org/react";
+import { Checkbox, Button, Spacer } from "@nextui-org/react";
 
-const ShoppingItem = ({ data }) => {
+const ShoppingItem = ({ data, onClick }) => {
   return (
-    <li style={{listStyleType: "none"}}>
-      <Checkbox line checked={false}>
-        {data.quantity}
+    <li style={{display: "flex", alignItems: "center", listStyleType: "none"}}>
+      <Checkbox line checked={data.needToBuy}>
+        {data.quantity}{" "}
         {data.ingredient}
       </Checkbox>
+      <Spacer x={0.5}/>
+      <Button onClick={onClick} size="xs" color="error" ghost auto>X</Button>
     </li>
   );
 };
 
-export default ShoppingItem;
+export default ShoppingItem
+;
--- pages/shopping_list/[email].js
+++ pages/shopping_list/[email].js
@@ -3,33 +3,33 @@ import NavBar from "../../Components/NavBar";
 import ShoppingItem from "../../Components/ShoppingItem";
 import { useUser } from "@auth0/nextjs-auth0";
 
-function ShoppingList({data}) {
-  // const [user] = useUser()
-  console.log(data)
+function ShoppingList({ data }) {
+  
+
+  function deleteItem() {
+    console.log("deleteItem was called")
+  }
+
   return (
     <div>
       <NavBar />
-      <h2>Shopping List</h2>
-      <div>
+      <h3 style={{textAlign: "center"}}>Shopping List</h3>
+      <hr/>
+      <div style={{paddingLeft: "4vw"}}>
         {data.map((item, index) => {
-          return (
-            <>
-              <ShoppingItem key={index} data={item} />
-            </>
-          );
-        }
-        )}
+          return <ShoppingItem onClick={deleteItem} key={index} data={item}/>;
+        })}
       </div>
     </div>
   );
 }
 
 export async function getServerSideProps(context) {
-  const email = encodeURI(context.params.email)
+  const email = encodeURI(context.params.email);
   const fetchURL = `http://localhost:3002/shopping/${email}`;
   const res = await fetch(fetchURL);
-  const data = await res.json()
-  return {props: {data: data.payload[0].shoppingList}}
+  const data = await res.json();
+  return { props: { data: data.payload[0].shoppingList } };
 }
 
 export default ShoppingList;
