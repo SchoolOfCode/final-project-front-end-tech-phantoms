dff1ad4bfcc94a7027589bd8d3eccb30c3b14be6
eniabellver <eugeniabellver@gmail.com>
2022-03-09 10:33:57 +0000

    wip postCheckItem function

--- pages/shopping_list/[email].js
+++ pages/shopping_list/[email].js
@@ -2,28 +2,43 @@ import React, { useState } from "react";
 import NavBar from "../../Components/NavBar";
 import ShoppingItem from "../../Components/ShoppingItem";
 
-function ShoppingList({ data }) {
+function ShoppingList({ data, email }) {
   const [listData, setListData] = useState(data);
 
+  
   function deleteItem(ingredientName) {
     listData.forEach((listItem, index) => {
-      if (listItem.ingredient === ingredientName) { //find the ingredient in the shopping list
-        setListData([ //update listData state immutably
+      if (listItem.ingredient === ingredientName) {
+        //find the ingredient in the shopping list
+        setListData([
+          //update listData state immutably
           ...listData.slice(0, index), //remove matching item using slice
           ...listData.slice(index + 1, listData.length),
         ]);
       }
     });
   }
+  
+  async function postCheckItem(ingredientName) {
+    console.log(email)
+    const fetchURL = `http://localhost:3002/shopping/${email}/checkOff/${ingredientName}`
+    const res = await fetch(fetchURL);
+    const data = await res.json();
+    console.log(data)
+  }
 
-  function checkItem(ingredientName) {
+  async function checkItem(ingredientName) {
+    let itemToCheck
     const newList = listData.map((listItem) => {
-      if (listItem.ingredient === ingredientName) { //find the ingredient in the shopping list
+      if (listItem.ingredient === ingredientName) {
+        //find the ingredient in the shopping list
+        itemToCheck = listItem.ingredient;
         listItem = { ...listItem, needToBuy: !listItem.needToBuy }; //toggle boolean value
         return listItem; //return changed item
       }
       return listItem; //return unchanged items
     });
+    await postCheckItem(itemToCheck);
     setListData(newList);
   }
 
@@ -53,7 +68,7 @@ export async function getServerSideProps(context) {
   const fetchURL = `http://localhost:3002/shopping/${email}`;
   const res = await fetch(fetchURL);
   const data = await res.json();
-  return { props: { data: data.payload[0].shoppingList } };
+  return { props: { email: email, data: data.payload[0].shoppingList } };
 }
 
 export default ShoppingList;
