607a4b5ba056f97a6d686a7b6e12a9cd98648b76
Scott Wilson <swrs@hotmail.co.uk>
2022-03-10 16:31:07 +0000

    moved the recipe fetch up to the savedRecipeLayout, just cant resolve each promise with .json

--- Components/SavedRecipesLayout/index.js
+++ Components/SavedRecipesLayout/index.js
@@ -1,10 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SavedRecipeCard from "./SavedRecipeCard";
 import { Container, Card } from "@nextui-org/react";
 import css from "./style.module.css";
 
 function SavedRecipesLayout({ savedRecipesData }) {
   const [recipeIDs, setRecipeIDs] = useState(savedRecipesData);
+  const [recipeData, setRecipeData] = useState(null);
+
+  useEffect(() => {
+    async function getRecipeData() {
+      //fetch all of the recipe id data at outlineColor
+      //store all of that in recipeData
+      //map tyhrough recipeData and render the components we need
+      // when a user deletes something filter it from recipeData
+      const fetches = recipeIDs.map((recipeID) => {
+        return fetch(
+          `https://api.edamam.com/api/recipes/v2/${recipeID}?type=public&app_id=${process.env.NEXT_PUBLIC_EDAMAM_APP_ID}&app_key=${process.env.NEXT_PUBLIC_EDAMAM_APP_KEY}`
+        );
+      });
+      const allPromise = Promise.all([fetches]);
+      const res = await allPromise;
+      const data = res.map(async (resItem) => {
+        const test = await resItem.json();
+        return test;
+      });
+      console.log(data);
+      //cant resolve each promise with .json
+    }
+    getRecipeData();
+  }, []);
 
   function deleteSavedRecipe(recipeID) {
     const index = recipeIDs.indexOf(recipeID);
@@ -21,7 +45,7 @@ function SavedRecipesLayout({ savedRecipesData }) {
         <h3 className={css.title}>Saved Recipes</h3>
         <hr></hr>
       </div>
-      {recipeIDs.map((item, index) => {
+      {/* {recipeIDs.map((item, index) => {
         return (
           <div key={item}>
             <Container className={css.container}>
@@ -42,7 +66,7 @@ function SavedRecipesLayout({ savedRecipesData }) {
             </Container>
           </div>
         );
-      })}
+      })} */}
     </div>
   );
 }
