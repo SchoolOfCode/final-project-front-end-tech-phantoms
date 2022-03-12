d93c9e9bdc1394f0ffca5a54a2a138b4e7a6367e
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-03-12 21:49:19 +0000

    added loading icon to navBar

--- Components/NavBar/index.js
+++ Components/NavBar/index.js
@@ -2,11 +2,11 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { useUser } from "@auth0/nextjs-auth0";
-import { Button, Grid, Text, Spacer } from "@nextui-org/react";
+import { Button, Grid, Text, Spacer, Loading } from "@nextui-org/react";
 
 function NavBar() {
   const { user, isLoading, error } = useUser();
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading) return <Loading />;
   if (error) return <div>{error.message}</div>;
 
   if (user) {
